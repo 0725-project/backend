4b9381f5eda8809ba9936f9d94306245d24b5105
Kim Jun Young <rlawnsdud0422@gmail.com>
2025-07-26 11:53:34 +0900

    updated

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,7 +1,10 @@
 import { Module } from '@nestjs/common'
 import { AppController } from './app.controller'
+import { PostsModule } from './posts/posts.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
-    controllers: [AppController]
+    controllers: [AppController],
+    imports: [PostsModule, UsersModule],
 })
 export class AppModule {}
