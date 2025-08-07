8fc2762d61e2c4abcef5752170f95aff72530323
Kim Jun Young <rlawnsdud0422@gmail.com>
2025-08-07 20:25:41 +0900

    updated Backend: Main(Home) Page

--- frontend/src/app/components/Main/index.tsx
+++ frontend/src/app/components/Main/index.tsx
@@ -90,14 +90,13 @@ const MainPage = () => {
 
     return (
         <section className='w-full flex flex-col items-center mt-20 p-5'>
-            <div className='w-full max-w-xl flex flex-col items-center mb-20'>
+            <div className='w-full max-w-4xl flex flex-col items-center mb-20'>
                 <input
                     type='text'
                     value={search}
                     onChange={(e) => setSearch(e.target.value)}
-                    placeholder='검색어를 입력하세요...'
-                    className='w-full h-16 px-6 py-4 rounded-2xl border border-gray-300 shadow focus:outline-none focus:ring-2 focus:ring-blue-400'
-                    disabled
+                    placeholder='/? 를 입력하여 자세히 알아보기'
+                    className='w-full h-20 px-6 py-4 rounded-2xl border border-gray-300 shadow focus:outline-none focus:ring-2 focus:ring-blue-400 hover:scale-102 transition-transform duration-200'
                 />
             </div>
             {isLoading ? (
