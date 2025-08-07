710d8f4f65376f66d48ada2fd546edc88793faae
Kim Jun Young <rlawnsdud0422@gmail.com>
2025-08-07 16:01:31 +0900

    updated Frontend

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,6 +3,14 @@ import type { NextConfig } from 'next'
 const nextConfig: NextConfig = {
     output: 'standalone',
     devIndicators: false,
+    async rewrites() {
+        return [
+            {
+                source: '/api/:path*',
+                destination: process.env.NEXT_PUBLIC_API_URL + '/:path*',
+            },
+        ]
+    }
 }
 
 export default nextConfig
