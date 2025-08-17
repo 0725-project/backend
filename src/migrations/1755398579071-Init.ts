d9d8427b61d25df19b43b074883759e2877a8165
Kim Jun Young <rlawnsdud0422@gmail.com>
2025-08-17 11:48:43 +0900

    updated TypeORM

--- src/migrations/1755398579071-Init.ts
+++ src/migrations/1755398579071-Init.ts
@@ -1,7 +1,7 @@
 import { MigrationInterface, QueryRunner } from 'typeorm'
 
-export class Init1755259919680 implements MigrationInterface {
-    name = 'Init1755259919680'
+export class Init1755398579071 implements MigrationInterface {
+    name = 'Init1755398579071'
 
     public async up(queryRunner: QueryRunner): Promise<void> {
         await queryRunner.query(
@@ -21,9 +21,12 @@ export class Init1755259919680 implements MigrationInterface {
             `CREATE TABLE "posts" ("id" SERIAL NOT NULL, "title" character varying(255) NOT NULL, "content" text NOT NULL, "createdAt" TIMESTAMP WITH TIME ZONE NOT NULL DEFAULT now(), "topicLocalId" integer NOT NULL, "viewCount" integer NOT NULL DEFAULT '0', "commentCount" integer NOT NULL DEFAULT '0', "likeCount" integer NOT NULL DEFAULT '0', "ip" character varying(15), "authorId" integer, "topicId" integer, "likesId" integer, CONSTRAINT "PK_2829ac61eff60fcec60d7274b9e" PRIMARY KEY ("id"))`,
         )
         await queryRunner.query(`CREATE INDEX "IDX_a72a4814d9256fbb39bb12bffc" ON "posts" ("topicId", "id") `)
+        await queryRunner.query(
+            `CREATE TABLE "subscriptions" ("id" SERIAL NOT NULL, "followerId" integer, "followingId" integer, CONSTRAINT "PK_a87248d73155605cf782be9ee5e" PRIMARY KEY ("id"))`,
+        )
         await queryRunner.query(`CREATE TYPE "public"."users_role_enum" AS ENUM('0', '1')`)
         await queryRunner.query(
-            `CREATE TABLE "users" ("id" SERIAL NOT NULL, "username" character varying(32) NOT NULL, "nickname" character varying(32), "password" character varying(255) NOT NULL, "email" character varying(320) NOT NULL, "description" character varying(255), "profileImage" character varying(255), "role" "public"."users_role_enum" NOT NULL DEFAULT '1', "points" integer NOT NULL DEFAULT '0', "postCount" integer NOT NULL DEFAULT '0', "commentCount" integer NOT NULL DEFAULT '0', "createdAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "UQ_fe0bb3f6520ee0469504521e710" UNIQUE ("username"), CONSTRAINT "UQ_97672ac88f789774dd47f7c8be3" UNIQUE ("email"), CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`,
+            `CREATE TABLE "users" ("id" SERIAL NOT NULL, "username" character varying(32) NOT NULL, "nickname" character varying(32), "password" character varying(255) NOT NULL, "email" character varying(320) NOT NULL, "description" character varying(255), "profileImage" character varying(255), "role" "public"."users_role_enum" NOT NULL DEFAULT '1', "points" integer NOT NULL DEFAULT '0', "postCount" integer NOT NULL DEFAULT '0', "commentCount" integer NOT NULL DEFAULT '0', "followersCount" integer NOT NULL DEFAULT '0', "followingCount" integer NOT NULL DEFAULT '0', "createdAt" TIMESTAMP NOT NULL DEFAULT now(), CONSTRAINT "UQ_fe0bb3f6520ee0469504521e710" UNIQUE ("username"), CONSTRAINT "UQ_97672ac88f789774dd47f7c8be3" UNIQUE ("email"), CONSTRAINT "PK_a3ffb1c0c8416b9fc6f907b7433" PRIMARY KEY ("id"))`,
         )
         await queryRunner.query(
             `ALTER TABLE "favorite_topics" ADD CONSTRAINT "FK_0169d16f5ecfc0fa4ddffb580a9" FOREIGN KEY ("userId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
@@ -55,9 +58,17 @@ export class Init1755259919680 implements MigrationInterface {
         await queryRunner.query(
             `ALTER TABLE "posts" ADD CONSTRAINT "FK_5497a85000a53c152b3432e75c3" FOREIGN KEY ("likesId") REFERENCES "likes"("id") ON DELETE NO ACTION ON UPDATE NO ACTION`,
         )
+        await queryRunner.query(
+            `ALTER TABLE "subscriptions" ADD CONSTRAINT "FK_22040b67c0262140a92dd38e7fe" FOREIGN KEY ("followerId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
+        )
+        await queryRunner.query(
+            `ALTER TABLE "subscriptions" ADD CONSTRAINT "FK_c08927026d5de9b1a1cb6b87d3d" FOREIGN KEY ("followingId") REFERENCES "users"("id") ON DELETE CASCADE ON UPDATE NO ACTION`,
+        )
     }
 
     public async down(queryRunner: QueryRunner): Promise<void> {
+        await queryRunner.query(`ALTER TABLE "subscriptions" DROP CONSTRAINT "FK_c08927026d5de9b1a1cb6b87d3d"`)
+        await queryRunner.query(`ALTER TABLE "subscriptions" DROP CONSTRAINT "FK_22040b67c0262140a92dd38e7fe"`)
         await queryRunner.query(`ALTER TABLE "posts" DROP CONSTRAINT "FK_5497a85000a53c152b3432e75c3"`)
         await queryRunner.query(`ALTER TABLE "posts" DROP CONSTRAINT "FK_7053388f60ec8ee652b912bcc7e"`)
         await queryRunner.query(`ALTER TABLE "posts" DROP CONSTRAINT "FK_c5a322ad12a7bf95460c958e80e"`)
@@ -70,6 +81,7 @@ export class Init1755259919680 implements MigrationInterface {
         await queryRunner.query(`ALTER TABLE "favorite_topics" DROP CONSTRAINT "FK_0169d16f5ecfc0fa4ddffb580a9"`)
         await queryRunner.query(`DROP TABLE "users"`)
         await queryRunner.query(`DROP TYPE "public"."users_role_enum"`)
+        await queryRunner.query(`DROP TABLE "subscriptions"`)
         await queryRunner.query(`DROP INDEX "public"."IDX_a72a4814d9256fbb39bb12bffc"`)
         await queryRunner.query(`DROP TABLE "posts"`)
         await queryRunner.query(`DROP TABLE "likes"`)
