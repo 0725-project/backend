dfb17f85ff3507c0869344e4233193fc5045513d
Kim Jun Young <rlawnsdud0422@gmail.com>
2025-08-25 15:46:09 +0900

    updated RabbitMQ Module

--- src/common/rabbitmq/rabbitmq.service.ts
+++ src/common/rabbitmq/rabbitmq.service.ts
@@ -1,28 +1,22 @@
 import { Injectable, OnModuleInit } from '@nestjs/common'
-import { Channel, connect } from 'amqplib'
+import { connect as amqpConnect, ChannelWrapper } from 'amqp-connection-manager'
+import { Channel } from 'amqplib'
 
 @Injectable()
 export class RabbitMQService implements OnModuleInit {
-    private channel: Channel
-    private readonly exchange = 'posts_exchange'
-    private readonly notificationQueue = 'notification_queue'
-    private readonly searchIndexQueue = 'searchindex_queue'
-    private readonly routingKey = 'post.created'
+    private channel!: ChannelWrapper
 
     async onModuleInit() {
-        const connection = await connect(process.env.RABBITMQ_URL || 'amqp://localhost:5672')
-        this.channel = await connection.createChannel()
-        await this.channel.assertExchange(this.exchange, 'topic', { durable: true })
-
-        await this.channel.assertQueue(this.notificationQueue, { durable: true })
-        await this.channel.assertQueue(this.searchIndexQueue, { durable: true })
-
-        await this.channel.bindQueue(this.notificationQueue, this.exchange, this.routingKey)
-        await this.channel.bindQueue(this.searchIndexQueue, this.exchange, this.routingKey)
+        const connection = amqpConnect([process.env.RABBITMQ_URL ?? 'amqp://localhost:5672'])
+        this.channel = connection.createChannel({
+            json: true,
+            setup: async (channel: Channel) => {
+                await channel.assertExchange('posts_exchange', 'topic', { durable: true })
+            },
+        })
     }
 
-    async publishPostCreated(data: any) {
-        if (!this.channel) throw new Error('RabbitMQ channel not initialized')
-        this.channel.publish(this.exchange, this.routingKey, Buffer.from(JSON.stringify(data)), { persistent: true })
+    async publishPostCreated(data: unknown) {
+        await this.channel.publish('posts_exchange', 'post.created', data, { persistent: true })
     }
 }
